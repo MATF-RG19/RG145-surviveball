d98b238e65337fdb4da6d578e7faf22ad4f7ccf5
boza.mitrovic@gmail.com <boza.mitrovic@gmail.com>
2020-08-15 00:19:49 +0200

    Adding lightng effects and ball movement

--- SurviveBall/SurviveBall/Constansts.h
+++ SurviveBall/SurviveBall/Constansts.h
@@ -1,10 +1,31 @@
+#define PI 3.14159265359
+
 const int WINDOW_HEIGHT = 600;
 const int WINDOW_WIDTH = 600;
 const int WINDOW_POSITION_X = 100;
 const int WINDOW_POSITION_Y = 100;
-const int KEY_ESC = 27;
-const int KEY_LEFT = 65;
-const int KEY_RIGHT = 68;
-const int KEY_JUMP = 32;
-const int KEY_TURBO = 87;
+
+const unsigned char KEY_ESC = 27;
+const unsigned char KEY_LEFT = 'a';
+const unsigned char KEY_RIGHT = 'd';
+const unsigned char KEY_JUMP = 32;
+const unsigned char KEY_TURBO = 87;
+const unsigned char KEY_START = 'g';
+
 const int MOVEMENT = 2;
+
+double BALL_X = 2;
+double BALL_Y = 1;
+double BALL_Z = -2;
+
+double PLANE_SCALE_X = 10;
+double PLANE_SCALE_Y = 1;
+double PLANE_SCALE_Z = 30;
+
+double SHIFT_X = 0;
+double SHIFT_Y = 0;
+
+float animation_parameter = 0;
+float animation_ongoing = 0;
+
+float JUMP_TIMEOUT = 0;
--- SurviveBall/SurviveBall/Main.cpp
+++ SurviveBall/SurviveBall/Main.cpp
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <GL/glut.h>
 #include <math.h>
+#include <iostream>
 #include "Constansts.h"
+using namespace std;
 
 static int timer_active;
-
 
 static void on_keyboard(unsigned char key, int x, int y);
 static void on_timer(int value);
@@ -31,6 +32,26 @@ int main(int argc, char** argv) {
     glClearColor(0.596, 1, 0.596, 1);
     glEnable(GL_DEPTH_TEST);
 
+    GLfloat light_position[] = { 2, 5, 20, 0 };
+
+    GLfloat light_ambient[] = { 0, 0, 0, 1 };
+
+    GLfloat light_diffuse[] = { 1, 1, 1, 1 };
+
+    GLfloat light_specular[] = { 1, 1, 1, 1 };
+
+    GLfloat model_ambient[] = { 0.4, 0.4, 0.4, 1 };
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, model_ambient);
+
+    glEnable(GL_COLOR_MATERIAL);
+
     glutMainLoop();
 
     return 0;
@@ -38,27 +59,49 @@ int main(int argc, char** argv) {
 
 static void on_keyboard(unsigned char key, int x, int y) {
     switch (key) {
-    case KEY_ESC:
-        exit(0);
-        break;
 
-    case KEY_LEFT:
-        break;
+        case KEY_ESC:
+            exit(0);
+            break;
 
-    case KEY_RIGHT:
-        break;
+        case KEY_START:
+            if (!animation_ongoing) {
+                animation_ongoing = 1;
+                glutTimerFunc(20, on_timer, 0);
+            }
+            break;
+
+        case KEY_LEFT:
+            SHIFT_X += 0.5;
+            break;
+
+        case KEY_RIGHT:
+            SHIFT_X -= 0.5;
+            break;
+
+        case KEY_JUMP:
+            if (SHIFT_Y == 0) {
+                JUMP_TIMEOUT = 20;
+            }
+            
+            break;
 
     }
 }
 
-static void on_timer(int value) {
+static void on_timer(int value){
     if (value != 0)
-        return;
+    return;
+
+    if (JUMP_TIMEOUT >= 0) {
+        SHIFT_Y = sin(JUMP_TIMEOUT*PI / 90);
+        JUMP_TIMEOUT -= 1;
+    }
 
     glutPostRedisplay();
 
-    if (timer_active)
-        glutTimerFunc(50, on_timer, 0);
+    if (animation_ongoing)
+        glutTimerFunc(20, on_timer, 0);
 }
 
 static void on_reshape(int width, int height) {
@@ -76,7 +119,7 @@ static void draw_plane() {
     glPushMatrix();
         glColor3f(0.29, 0.447, 0.584);
         glTranslatef(2, -0.100, 2);
-        glScalef(10, 1, 30);
+        glScalef(5, 1, 100);
         glutSolidCube(1);
     glPopMatrix();
 }
@@ -84,7 +127,7 @@ static void draw_plane() {
 static void draw_ball() {
     glPushMatrix();
         glColor3f(1, 1, 1);
-        glTranslatef(2, 1, -2);
+        glTranslatef(BALL_X, BALL_Y, BALL_Z);
         glutSolidSphere(0.3, 100, 100);
     glPopMatrix();
 }
@@ -95,11 +138,15 @@ static void on_display(void) {
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(2, 2, -5,
-              2, 0, 2,
+    gluLookAt(2 + SHIFT_X, 2, -5,
+              2 + SHIFT_X, 0, 2,
               0, 1, 0);
     draw_plane();
-    draw_ball();
+
+    glPushMatrix();
+        glTranslatef(SHIFT_X, SHIFT_Y, 0);
+        draw_ball();
+    glPopMatrix();
 
     glutSwapBuffers();
 }
